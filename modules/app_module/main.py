5f6a0245b02695ab07857a22bf66626e979d7ece
Luka Triska <triska@ucu.edu.ua>
2017-06-12 18:21:29 +0300

    correct spelling mistake

--- modules/app_module/main.py
+++ modules/app_module/main.py
@@ -1,7 +1,7 @@
 from modules import data_collecting, data_processing
 from ADT import weather_ADT_test
 
-file = input("Which file fo you want to run? data_collecting - dc; data_processing - dp; WeatherADT_test - wat: ")
+file = input("Which file do you want to run? data_collecting - dc; data_processing - dp; WeatherADT_test - wat: ")
 
 if file == 'dc':
     data_collecting.main()
